2fcae61f1622f85867adacc9e8ffd8b48ec539c2
sethpolley <43558706+sethpolley@users.noreply.github.com>
2022-02-16 14:25:36 -0600

    Delete RetrievalCode.py
    This working segment of code was added to the larger program.

--- RetrievalCode.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-# import module 
-local_copy = 'http_access_log'
-# define log file variable
-
-local_copy, headers = urllib.request.urlretrieve('https://s3.amazonaws.com/tcmg476/http_access_log', local_copy)
-
-# create local file called 'http_access_log' with data from the URL provided
