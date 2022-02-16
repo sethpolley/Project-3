581841a4123bb780efe82012e9db895257196ac4
Seth Polley <aggieseth@tamu.edu>
2022-02-16 13:27:11 -0600

    Finally worked.

--- RetrievalCode.py
+++ RetrievalCode.py
@@ -1,1 +1,9 @@
-This is where the retrieval code will be written
+import urllib.request
+
+# import module 
+local_copy = 'http_access_log'
+# define log file variable
+
+local_copy, headers = urllib.request.urlretrieve('https://s3.amazonaws.com/tcmg476/http_access_log', local_copy)
+
+# create local file called 'http_access_log' with data from the URL provided
